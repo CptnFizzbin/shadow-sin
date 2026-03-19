3a2bd401efee544fa3386ba5cfc09d9d1ff5b0be
Captain Fizzbin <code@cptnfizzbin.ca>
2026-03-19 19:14:40 -0400

    feat: switch to hash history for router (#10)

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,16 @@
-import { createRouter as createTanStackRouter } from "@tanstack/react-router"
+import {
+  createHashHistory,
+  createRouter as createTanStackRouter,
+} from "@tanstack/react-router"
 import { getContext } from "./integrations/tanstack-query/root-provider.tsx"
 import { routeTree } from "./routeTree.gen.ts"
 
 export function getRouter() {
+  const hashHistory = createHashHistory()
+
   return createTanStackRouter({
     routeTree,
+    history: hashHistory,
 
     context: getContext(),
 
