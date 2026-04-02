ceee9c3a21eddab328a7bcfd1ed54e42693f931d
Captain Fizzbin <code@cptnfizzbin.ca>
2026-04-01 21:25:41 -0400

    refactor: update alert imports and layout adjustments in RootLayout

--- src/components/CharacterBuilder/Sections/Resources/Technomancer/UseTechnomancerAlerts.ts
+++
@@ -1,9 +0,0 @@
-import { useComplexFormsAlerts } from "#/components/CharacterBuilder/Sections/Resources/Technomancer/UseComplexFormsAlerts.ts"
-import { useSpritesAlerts } from "#/components/CharacterBuilder/Sections/Resources/Technomancer/UseSpritesAlerts.ts"
-
-export const useTechnomancerAlerts = () => {
-  return [
-    ...useComplexFormsAlerts(),
-    ...useSpritesAlerts(),
-  ]
-}
