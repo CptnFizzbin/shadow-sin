e07ebdc6e9db05319dee8e44e3db41f92faf5c49
Captain Fizzbin <code@cptnfizzbin.ca>
2026-04-21 21:25:55 -0400

    refactor: replace Counter components with CounterField in armor and damage sections

--- src/components/gear/armor/forms/armorFormFields.tsx
+++ src/components/gear/armor/forms/armorFormFields.tsx
@@ -1,9 +1,6 @@
-import Paper from "@mui/material/Paper"
 import Stack from "@mui/material/Stack"
 
-import {
-  armorFormOpts,
-} from "#/components/gear/armor/forms/useArmorForm.tsx"
+import { armorFormOpts } from "#/components/gear/armor/forms/useArmorForm.tsx"
 import { Label } from "#/components/ui/text/label.tsx"
 import { withFieldGroup } from "#/integrations/tanstackForm/useAppForm.ts"
 
@@ -11,21 +8,19 @@ export const ArmorFormFields = withFieldGroup({
   ...armorFormOpts,
   render: ({ group }) => {
     return (
-      <Paper sx={{ padding: 1 }}>
-        <Stack>
-          <Label label="Armor Ratings" />
+      <>
+        <Label label="Armor Ratings" />
 
-          <Stack direction="row" sx={{ gap: 1 }}>
-            <group.AppField name="ballistic">
-              {(field) => <field.CounterField label="Ballistic" min={0} max={20} />}
-            </group.AppField>
+        <Stack direction="row" sx={{ gap: 1 }}>
+          <group.AppField name="ballistic">
+            {(field) => <field.CounterField label="Ballistic" min={0} max={20} fullWidth />}
+          </group.AppField>
 
-            <group.AppField name="impact">
-              {(field) => <field.CounterField label="Impact" min={0} max={20} />}
-            </group.AppField>
-          </Stack>
+          <group.AppField name="impact">
+            {(field) => <field.CounterField label="Impact" min={0} max={20} fullWidth />}
+          </group.AppField>
         </Stack>
-      </Paper>
+      </>
     )
   },
 })
