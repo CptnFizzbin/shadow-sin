3d9c7c82d005a1fe9cb5ead67ee7f0a8ee034e11
Captain Fizzbin <code@cptnfizzbin.ca>
2026-06-14 09:51:56 -0400

    yarn fix

--- src/components/character/karma/spendKarmaDialog.tsx
+++ src/components/character/karma/spendKarmaDialog.tsx
@@ -1,5 +1,6 @@
-import { SpendKarmaDialogContent } from "./spendKarmaDialogContent.tsx"
 import { useDialogApi } from "#/components/ui/dialog/api/dialogApiProvider.tsx"
+
+import { SpendKarmaDialogContent } from "./spendKarmaDialogContent.tsx"
 
 export const useSpendKarmaDialog = () => {
   const dialogApi = useDialogApi()
