bd8bbc00c616899fab1b5a43ac9e32ed5728d4aa
Captain Fizzbin <code@cptnfizzbin.ca>
2026-09-10 09:43:32 -0400

    Create workflow primitive (#591)

---
+++ src/components/ui/workflow/workflowProvider.tsx
@@ -0,0 +1,47 @@
+import type { PropsWithChildren } from "react"
+import { useMemo, useReducer } from "react"
+
+import type { Workflow } from "./createWorkflow.tsx"
+import type { WorkflowCtrl } from "./workflowContext.ts"
+import { workflowReducer } from "./workflowReducer.tsx"
+
+interface WorkflowProviderProps<TData extends object, TSteps extends string> extends PropsWithChildren {
+  workflow: Workflow<TData, TSteps>
+  initialData: TData
+  initialStep: TSteps
+  previousSteps?: TSteps[]
+}
+
+export const WorkflowProvider = <TData extends object, TSteps extends string>(props: WorkflowProviderProps<TData, TSteps>) => {
+  const { workflow, initialData, initialStep, previousSteps = [], children } = props
+  const Provider = workflow.context.Provider
+
+  const [state, dispatch] = useReducer(workflowReducer, {
+    currentStep: initialStep,
+    previousSteps: previousSteps,
+    data: initialData,
+  })
+
+  const ctrl = useMemo((): WorkflowCtrl<TData, TSteps> => ({
+    setData: (dataOrUpdater: TData | ((data: TData) => TData)) => {
+      if (typeof dataOrUpdater === "function") {
+        const updater = dataOrUpdater
+        dispatch({ type: "updateData", updater })
+      } else {
+        const data = dataOrUpdater
+        dispatch({ type: "setData", data })
+      }
+    },
+    back: () => void dispatch({ type: "previousStep" }),
+    next: (step: TSteps) => void dispatch({ type: "nextStep", step }),
+  }), [dispatch])
+
+  const contextValue = useMemo(() => ({
+    ...state,
+    ...ctrl,
+  }), [state, ctrl])
+
+  return (
+    <Provider value={contextValue}>{children}</Provider>
+  )
+}
---
+++ src/components/ui/workflow/workflowReducer.tsx
@@ -0,0 +1,36 @@
+import { produce } from "immer"
+
+import type { WorkflowState } from "./workflowContext.ts"
+
+export type WorkflowAction<TData extends object, TSteps extends string> =
+  | { type: "updateData", updater: (data: TData) => TData }
+  | { type: "setData", data: TData }
+  | { type: "nextStep", step: TSteps }
+  | { type: "previousStep" }
+  | { type: "reset", state: WorkflowState<TData, TSteps> }
+
+export const workflowReducer = <TData extends object, TSteps extends string>(state: WorkflowState<TData, TSteps>, action: WorkflowAction<TData, TSteps>) => {
+  return produce(state, (draft): WorkflowState<TData, TSteps> => {
+    switch (action.type) {
+      case "previousStep":
+        if (draft.previousSteps.length >= 1) {
+          draft.currentStep = draft.previousSteps.pop()!
+        }
+        break
+      case "setData":
+        draft.data = action.data
+        break
+      case "updateData":
+        draft.data = produce(draft.data, action.updater)
+        break
+      case "nextStep":
+        draft.previousSteps.push(draft.currentStep)
+        draft.currentStep = action.step
+        break
+      case "reset":
+        return action.state
+    }
+
+    return draft
+  })
+}
